a7fd16bf3efd74ea770837e27520f6006a0b792f
Moe Master <finalfanleisy@gmail.com>
2025-03-27 13:29:31 -0500

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,48 @@
+import pysam
+from apache_beam.io import gcsio
+import apache_beam as beam
+from io import BytesIO
+
+# Input and output paths
+gcs_path = 'gs://your-bucket/path/to/original.bam'
+output_path = 'gs://your-bucket/path/to/modified.bam'
+
+# Step 1 & 3: Read and modify header, get alignment start
+with gcsio.GcsIO().open(gcs_path, 'rb') as f:
+    bam = pysam.AlignmentFile(f, 'rb')
+    header = bam.header
+    alignment_start = f.tell()
+    header_dict = header.to_dict()
+    # Redact metadata (example)
+    if 'PG' in header_dict:
+        del header_dict['PG']
+    new_header = pysam.AlignmentHeader.from_dict(header_dict)
+    header_buf = BytesIO()
+    temp_bam = pysam.AlignmentFile(header_buf, 'wb', header=new_header)
+    temp_bam.close()
+    new_header_bytes = header_buf.getvalue()
+
+# Step 5: Beam pipeline to write new BAM file
+class WriteRedactedBamFn(beam.DoFn):
+    def __init__(self, original_path, alignment_start, new_header_bytes, output_path):
+        self.original_path = original_path
+        self.alignment_start = alignment_start
+        self.header_bytes = new_header_bytes
+        self.output_path = output_path
+
+    def process(self, element):
+        with gcsio.GcsIO().open(self.output_path, 'wb') as out_f:
+            out_f.write(self.header_bytes)
+            with gcsio.GcsIO().open(self.original_path, 'rb') as in_f:
+                in_f.seek(self.alignment_start)
+                while True:
+                    chunk = in_f.read(1024 * 1024)
+                    if not chunk:
+                        break
+                    out_f.write(chunk)
+        yield self.output_path
+
+with beam.Pipeline(runner='DataflowRunner', options=beam.options.pipeline_options.PipelineOptions()) as p:
+    (p 
+     | beam.Create([None])
+     | beam.ParDo(WriteRedactedBamFn(gcs_path, alignment_start, new_header_bytes, output_path)))
